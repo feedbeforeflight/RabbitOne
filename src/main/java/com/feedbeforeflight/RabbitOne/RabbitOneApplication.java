87ebe3b319d47062f6b2cb518f53efed8ab11bdf
Igor Zosimovich <igor.zosimovich@gmail.com>
2020-11-21 08:28:52 +0500

    Initial commit

---
+++ src/main/java/com/feedbeforeflight/RabbitOne/RabbitOneApplication.java
@@ -0,0 +1,16 @@
+package com.feedbeforeflight.RabbitOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitOneApplication.class, args);
+
+		System.out.println("It works!");
+		System.out.println("It works!");
+	}
+
+}
---
+++ src/test/java/com/feedbeforeflight/RabbitOne/RabbitOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feedbeforeflight.RabbitOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
